cd97b4cd24acff43d8d5dd2171cda33ad31f3615
Arham Abiyan <arham.abiyan@gmail.com>
2020-09-19 20:37:35 +0700

    get by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create.dto';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
@@ -11,6 +11,11 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
   }
 
   @Post()
